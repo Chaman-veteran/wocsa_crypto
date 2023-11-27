9f2ae3d8f91bdcac9f8e6f7abc356278fcb5f324
Chaman-veteran <maillen@bde.enseeiht.fr>
2023-11-27 15:00:13 +0100

    chall rsa_1

--- rsa_1/rsa_1.py
+++ rsa_1/rsa_1.py
@@ -1,4 +1,5 @@
-from Crypto.Util.number import getPrime
+from Crypto.Util.number import getPrime, inverse
+from Crypto.PublicKey import RSA
 from math import gcd
 
 e = 65537
@@ -8,6 +9,12 @@ while gcd(e,(p-1)*(q-1)) != 1:
     p = getPrime(100)
     q = getPrime(100)
 
+d = inverse(e,(p-1)*(q-1))
 N = p*q
 
-print(N)
+K = RSA.construct((N,e,d))
+with open('rsa_1/privkey.pem','wb') as f:
+    f.write(K.export_key())
+
+with open('rsa_1/pubkey.pem','wb') as f:
+    f.write(K.public_key().export_key())
