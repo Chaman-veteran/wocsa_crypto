be2bf59e9aadf1c8d0099d92a2dd7fbfd6c40d83
Édouard Lefizelier <edouardlefi@gmail.com>
2023-11-29 23:14:23 +0100

    ajout de la solution du OTP

---
+++ xor_1/solution.py
@@ -0,0 +1,18 @@
+ciphertext = open('ciphertext_bytes', 'rb').read()
+var_key = ""
+dict = {}
+
+# On test pour des tailles de clef allant de 1 a 10
+for LEN_KEY in range(1,10):
+    for k in range(0, LEN_KEY):
+        # Deux lettres espacées de LEN_KEY sont xorées avec la même lettre de la clef
+        for i in range(k, len(ciphertext), LEN_KEY):
+            if ciphertext[i] in dict:
+                dict[ciphertext[i]] += 1
+            else:
+                dict[ciphertext[i]] = 1
+        # L'espace a des chances d'être le caractère le plus fréquent
+        var_key += chr((max(dict, key=dict.get))^ord(' '))
+        dict.clear()
+    print(var_key)
+    var_key = ""
